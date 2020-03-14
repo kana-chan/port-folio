8d2da6fb38369305e6fcb8510a9166a7ffacca36
kana-chan <bkm0115_y@icloud.com>
2020-03-14 21:43:47 +0900

    menumodal

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,8 +1,10 @@
 $(function(){
   $('.menuIcon').on('click',function(){
     $('.menuModal').fadeIn();
+    $('.menuModalWrapper').fadeIn();
   });
   $('.closeIcon').on('click',function(){
     $('.menuModal').fadeOut();
+    $('.menuModalWrapper').fadeOut();
   });
 });
