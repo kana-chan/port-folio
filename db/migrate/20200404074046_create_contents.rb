820bab501aa937edb96c1bf0a03d4e2579b1872a
kana-chan <bkm0115_y@icloud.com>
2020-04-04 21:21:11 +0900

    add culoum

--- db/migrate/20200404074046_create_contents.rb
+++ db/migrate/20200404074046_create_contents.rb
@@ -2,6 +2,7 @@ class CreateContents < ActiveRecord::Migration[5.0]
   def change
     create_table :contents do |t|
       t.string :name,                   null: false
+      t.string :link
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20200404074532) do
 
   create_table "contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
+    t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
