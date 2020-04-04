253d38f419b96a001d487e1adea95c7cfb4e3e2a
kana-chan <bkm0115_y@icloud.com>
2020-04-04 16:22:56 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "contents#index"
-  resources :contents, only: :index
+  resources :contents, only: [:index, :show]
   resources :profiles, only: :index
 end
