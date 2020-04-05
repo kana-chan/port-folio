d1ca7164295c88357e0cef3b209a1d10795ce16c
kana-chan <bkm0115_y@icloud.com>
2020-04-05 23:27:37 +0900

    Update contents_controller.rb

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -6,4 +6,23 @@ class ContentsController < ApplicationController
   def show
     @content = Content.find(params[:id])
   end
+
+  def new
+    @content = Content.new
+  end
+
+  def create
+    @content = Content.new(content_params)
+    @content.save
+  end
+
+  private
+  def content_params
+    params.require(:content).permit(
+      :name,
+      :image,
+      :text,
+      :link,
+    )
+  end
 end
