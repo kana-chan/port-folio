86b69d355ea7806b36ca0e89f7c5c4cb9452b099
kana-chan <bkm0115_y@icloud.com>
2020-04-06 19:55:53 +0900

    varidate

--- db/migrate/20200404122644_create_categories.rb
+++ db/migrate/20200404122644_create_categories.rb
@@ -1,8 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[5.0]
   def change
     create_table :categories do |t|
-      t.string :name
-      t.string :color
+      t.string :name,                  null: false, unique: true
+      t.string :color,                 null: false, unique: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_04_04_124807) do
 
   create_table "categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "color"
+    t.string "name", null: false
+    t.string "color", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
