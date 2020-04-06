8196fac8a2c5027e4f2040908d8a48be04c40edc
kana-chan <bkm0115_y@icloud.com>
2020-04-06 18:13:32 +0900

    Update content.rb

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,6 +2,7 @@ class Content < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   has_many :infomations
+    accepts_nested_attributes_for :infomations,                       allow_destroy: true
   has_many :content_categories
     has_many :categories, through: :content_categories
 end
