79ff77b263a9d32704c43464423edc32958472b0
kana-chan <bkm0115_y@icloud.com>
2020-04-06 20:16:27 +0900

    validates

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   has_many :content_categories
     has_many :contents, through: :content_categories 
+
+  validates :name,                      uniqueness: true
+  validates :color,                     uniqueness: true
 end
