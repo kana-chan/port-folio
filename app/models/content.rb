831b69bd324bca7ca65e1f0452a1356bc3524451
kana-chan <bkm0115_y@icloud.com>
2020-04-06 09:59:03 +0900

    gem carriewave

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,6 @@
 class Content < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   has_many :infomations
   has_many :content_categories
     has_many :categories, through: :content_categories
