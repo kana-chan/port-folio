5238bd3aad782f8a191fc08fd57454dbb2005991
kana-chan <bkm0115_y@icloud.com>
2020-04-06 13:19:09 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200404124807) do
+ActiveRecord::Schema.define(version: 2020_04_04_124807) do
 
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.string   "color"
+  create_table "categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "content_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "content_id"
-    t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["category_id"], name: "index_content_categories_on_category_id", using: :btree
-    t.index ["content_id"], name: "index_content_categories_on_content_id", using: :btree
+  create_table "content_categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "content_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_content_categories_on_category_id"
+    t.index ["content_id"], name: "index_content_categories_on_content_id"
   end
 
-  create_table "contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.string   "image",      null: false
-    t.string   "text",       null: false
-    t.string   "link",       null: false
+  create_table "contents", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image", null: false
+    t.string "text", null: false
+    t.string "link", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "infomations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "image",      null: false
-    t.string   "text",       null: false
-    t.string   "heading",    null: false
-    t.integer  "content_id"
+  create_table "infomations", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.string "text", null: false
+    t.string "heading", null: false
+    t.integer "content_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["content_id"], name: "index_infomations_on_content_id", using: :btree
+    t.index ["content_id"], name: "index_infomations_on_content_id"
   end
 
   add_foreign_key "content_categories", "categories"
