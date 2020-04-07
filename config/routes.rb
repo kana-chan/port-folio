dd33e80f0bf59ac9ce0cb3a6c2c06df0e3393439
kana-chan <bkm0115_y@icloud.com>
2020-04-07 13:35:15 +0900

    refacterring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "contents#index"
-  resources :contents, only: [:index, :show, :new, :create, :edit, :update]
+  resources :contents
   resources :categories, only: [:new, :create]
   resources :profiles, only: :index
 end
