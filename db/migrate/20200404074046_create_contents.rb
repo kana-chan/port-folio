a56ae14103d66f3952f99b5f260f8bf7bb6c3554
kana-chan <bkm0115_y@icloud.com>
2020-04-08 13:36:04 +0900

    change db

--- db/migrate/20200404074046_create_contents.rb
+++ db/migrate/20200404074046_create_contents.rb
@@ -2,8 +2,6 @@ class CreateContents < ActiveRecord::Migration[5.0]
   def change
     create_table :contents do |t|
       t.string :name,                   null: false
-      t.string :image,                  null: false
-      t.string :text,                   null: false
       t.string :link,                   null: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 2020_04_04_124807) do
 
   create_table "contents", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "image", null: false
-    t.string "text", null: false
     t.string "link", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
