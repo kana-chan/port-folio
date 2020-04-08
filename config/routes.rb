ed83ad9019c632646cf98db72ddc0266cc279fba
kana-chan <bkm0115_y@icloud.com>
2020-04-08 14:59:16 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "contents#index"
   resources :contents
-  resources :categories, only: [:new, :create]
+  resources :categories, only: [:new, :create, :edit, :update, :destroy]
   resources :profiles, only: :index
 end
