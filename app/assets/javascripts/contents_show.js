9cd1562f76c79a59d06894ae3397e0cc29bf56eb
kana-chan <bkm0115_y@icloud.com>
2020-04-09 18:37:57 +0900

    comit

--- app/assets/javascripts/contents_show.js
+++ app/assets/javascripts/contents_show.js
@@ -2,10 +2,20 @@ $(function(){
   $('.back').on('click',function(){
     console.log('back');
     
-  })
+  });
 
   $('.next').on('click',function(){
     console.log('next');
     
-  })
+  });
+
+  $('.btnWrapper--btn').on('click',function(){
+    var btnIndex = $(this).index();
+    $('.btnWrapper--btn')
+      .removeClass('active')
+      .eq(btnIndex).addClass('active');
+    $('.contentBox__image')
+      .removeClass('active')
+      .eq(btnIndex).addClass('active');
+  });
 })
