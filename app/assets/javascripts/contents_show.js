9c284e390232c567d9e421f908840c0d447ea14c
kana-chan <bkm0115_y@icloud.com>
2020-04-09 22:26:14 +0900

    comit

--- app/assets/javascripts/contents_show.js
+++ app/assets/javascripts/contents_show.js
@@ -1,21 +1,31 @@
 $(function(){
-  $('.back').on('click',function(){
-    console.log('back');
-    
-  });
-
-  $('.next').on('click',function(){
-    console.log('next');
-    
-  });
-
-  $('.btnWrapper--btn').on('click',function(){
-    var btnIndex = $(this).index();
-    $('.btnWrapper--btn')
-      .removeClass('active')
-      .eq(btnIndex).addClass('active');
-    $('.contentBox__image')
-      .removeClass('active')
-      .eq(btnIndex).addClass('active');
-  });
+  if (window.location.href.match(/\/contents\/\d+$/)) {
+// ------------------------------------------------
+    $('.next').on('click',function(){
+      var nowActive = $('.active')
+      nowActive
+        .removeClass('active')
+        .next().addClass('active');
+    });
+    $('.back').on('click',function(){
+      var nowActive = $('.active')
+      nowActive
+        .removeClass('active')
+        .prev().addClass('active');
+    });
+// ------------------------------------------------
+    $('.btnWrapper--btn').on('click',function(){
+      var btnIndex = $(this).index();
+      $('.btnWrapper--btn')
+        .removeClass('active')
+        .eq(btnIndex).addClass('active');
+      $('.contentBox__image')
+        .removeClass('active')
+        .eq(btnIndex).addClass('active');
+      if (btnIndex == 0){
+        $('.back').hide();
+      }
+    });
+// ------------------------------------------------
+  }
 })
