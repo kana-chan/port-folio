90c01fe2a6ea363535e376cbb784527d08a1bb19
kana-chan <bkm0115_y@icloud.com>
2020-04-10 11:03:58 +0900

    ok

--- app/assets/javascripts/contents_show.js
+++ app/assets/javascripts/contents_show.js
@@ -6,12 +6,23 @@ $(function(){
       nowActive
         .removeClass('active')
         .next().addClass('active');
+      if (nowActive.index() == 0 ){
+        $('.back').fadeIn();
+      }else if(nowActive.index() == 3){
+        $('.next').hide();
+      };
     });
+// ------------------------------------------------
     $('.back').on('click',function(){
       var nowActive = $('.active')
       nowActive
         .removeClass('active')
         .prev().addClass('active');
+      if (nowActive.index() == 4){
+        $('.next').fadeIn();
+      } else if (nowActive.index() == 1){
+        $('.back').hide();
+      }
     });
 // ------------------------------------------------
     $('.btnWrapper--btn').on('click',function(){
@@ -23,7 +34,14 @@ $(function(){
         .removeClass('active')
         .eq(btnIndex).addClass('active');
       if (btnIndex == 0){
-        $('.back').hide();
+        $('.back').fadeOut();
+        $('.next').fadeIn();
+      }else if (btnIndex == 4){
+        $('.back').fadeIn();
+        $('.next').fadeOut();
+      } else{
+        $('.back').fadeIn();
+        $('.next').fadeIn();
       }
     });
 // ------------------------------------------------
