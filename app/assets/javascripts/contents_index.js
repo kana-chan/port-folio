b2ec86fe059cf6bb3937c66b97d01f18d2c0b404
kana-chan <bkm0115_y@icloud.com>
2020-04-17 18:07:40 +0900

    linkbtn

--- app/assets/javascripts/contents_index.js
+++ app/assets/javascripts/contents_index.js
@@ -9,17 +9,31 @@ $(function(){
   });
 
 // contents================================================
-// hover------------------------------------------------
-
   $('.works__contentsBox__photoBlock').hover(
     function(){
       $(this).children('.works__contentsBox__photoBlock__label').animate({bottom:'0'},200)
-      // $(this).siblings('.works__contentsBox__separateText').animate({left:'85.6%'},200)
+      $(this).parents().next('.works__linkBox')
+        .css('backgroundColor','black')
+        .css('color','white')
     },
     function(){
       $(this).children('.works__contentsBox__photoBlock__label').animate({bottom:'-20vh'},200)
-      // $(this).siblings('.works__contentsBox__separateText').animate({left:'-15%'},200)
+      $(this).parents().next('.works__linkBox')
+      .css('backgroundColor','white')
+      .css('color','black')
     });
+// links================================================
+  $('.works__linkBox').hover(
+    function(){
+      $(this)
+        .css('backgroundColor','black')
+        .css('color','white')
+    },
+    function(){
+      $(this)
+        .css('backgroundColor','white')
+        .css('color','black')
+  });
 // ================================================
   }
 });
