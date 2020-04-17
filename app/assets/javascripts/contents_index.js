e5804d61999282d63c6bdd6205ef2339f53b35db
kana-chan <bkm0115_y@icloud.com>
2020-04-17 13:39:32 +0900

    ok

--- app/assets/javascripts/contents_index.js
+++ app/assets/javascripts/contents_index.js
@@ -25,25 +25,25 @@ $(function(){
   });
 // contents================================================
 // hover------------------------------------------------
-  $('.works__contentsBox').hover(
-    function(){
-      $(this).children('.works__contentsBox__filter').show()
-      $(this).children('.works__contentsBox__photoBlock').css('filter','blur(5px) grayscale(99%)')
-      $(this).children().children('.works__contentsBox__filter--more').show();
-    },
-    function(){
-      $(this).children('.works__contentsBox__filter')
-        .hide()
-        .css('width','100%');
-      $(this).children('.works__contentsBox__photoBlock').css('filter','');
-      $(this).children('.works__contentsBox__textBlock').css('right','-45%');
-    })
-    // more click------------------------------------------------
-    $('.works__contentsBox__filter--more').on('click',function(){
-      $(this).hide();
-      $(this).parents('.works__contentsBox__filter').animate({width:'55%'})
-      $(this).parents().siblings('.works__contentsBox__textBlock').animate({right:"0"});
-    })
+  // $('.works__contentsBox').hover(
+  //   function(){
+  //     $(this).children('.works__contentsBox__filter').show()
+  //     $(this).children('.works__contentsBox__photoBlock').css('filter','blur(3px)')
+  //     $(this).children().children('.works__contentsBox__filter--more').show();
+  //   },
+  //   function(){
+  //     $(this).children('.works__contentsBox__filter')
+  //       .hide()
+  //       .css('width','100%');
+  //     $(this).children('.works__contentsBox__photoBlock').css('filter','');
+  //     $(this).children('.works__contentsBox__textBlock').css('right','-45%');
+  //   })
+  //   // more click------------------------------------------------
+  //   $('.works__contentsBox__filter').on('click',function(){
+  //     $(this).children('.works__contentsBox__filter--more').hide();
+  //     $(this).animate({width:'55%'})
+  //     $(this).siblings('.works__contentsBox__textBlock').animate({right:"0"});
+  //   })
 // ================================================
   }
 });
