71289572ab19f97d26dbb1f85ed3cd2fcf41a67a
kana-chan <bkm0115_y@icloud.com>
2020-04-17 17:38:37 +0900

    update

--- app/assets/javascripts/contents_index.js
+++ app/assets/javascripts/contents_index.js
@@ -10,25 +10,16 @@ $(function(){
 
 // contents================================================
 // hover------------------------------------------------
-  // $('.works__contentsBox').hover(
-  //   function(){
-  //     $(this).children('.works__contentsBox__filter').show()
-  //     $(this).children('.works__contentsBox__photoBlock').css('filter','blur(3px)')
-  //     $(this).children().children('.works__contentsBox__filter--more').show();
-  //   },
-  //   function(){
-  //     $(this).children('.works__contentsBox__filter')
-  //       .hide()
-  //       .css('width','100%');
-  //     $(this).children('.works__contentsBox__photoBlock').css('filter','');
-  //     $(this).children('.works__contentsBox__textBlock').css('right','-45%');
-  //   })
-  //   // more click------------------------------------------------
-  //   $('.works__contentsBox__filter').on('click',function(){
-  //     $(this).children('.works__contentsBox__filter--more').hide();
-  //     $(this).animate({width:'55%'})
-  //     $(this).siblings('.works__contentsBox__textBlock').animate({right:"0"});
-  //   })
+
+  $('.works__contentsBox__photoBlock').hover(
+    function(){
+      $(this).children('.works__contentsBox__photoBlock__label').animate({bottom:'0'},200)
+      // $(this).siblings('.works__contentsBox__separateText').animate({left:'85.6%'},200)
+    },
+    function(){
+      $(this).children('.works__contentsBox__photoBlock__label').animate({bottom:'-20vh'},200)
+      // $(this).siblings('.works__contentsBox__separateText').animate({left:'-15%'},200)
+    });
 // ================================================
   }
 });
