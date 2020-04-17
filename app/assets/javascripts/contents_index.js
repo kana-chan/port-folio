47640f9a360ef09cc59b0057425289b76dde7eb3
kana-chan <bkm0115_y@icloud.com>
2020-04-17 18:15:22 +0900

    diteil link btn

--- app/assets/javascripts/contents_index.js
+++ app/assets/javascripts/contents_index.js
@@ -2,9 +2,10 @@ $(function(){
   if(window.location.href.match(/\/$/)) {
 // topの画像が読み込まれてから、スクロール可に(ログインボタンも消す)================================================
   $.when(
-    // $('.top__nameBox').fadeIn(4000)
+    $('.top__nameBox').fadeIn(4000)
   ).done(function(){
     $('.main').show();
+    $('#footer').show();
     $('.login').hide();
   });
 
