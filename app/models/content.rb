fc66da29eba1939c7958ee350c3d8433ce4fa08f
kana-chan <bkm0115_y@icloud.com>
2020-04-18 23:04:37 +0900

    uploader

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,6 @@
 class Content < ApplicationRecord
+  mount_uploader :overview, ImageUploader
+
   has_many :infomations,dependent: :delete_all 
     accepts_nested_attributes_for :infomations,allow_destroy: true
   has_many :content_categories,dependent: :delete_all 
