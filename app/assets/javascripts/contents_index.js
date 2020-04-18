1a51880374d8acdac0f5a36759cd8ac6daddf246
kana-chan <bkm0115_y@icloud.com>
2020-04-18 14:25:52 +0900

    setting view

--- app/assets/javascripts/contents_index.js
+++ app/assets/javascripts/contents_index.js
@@ -2,7 +2,7 @@ $(function(){
   if(window.location.href.match(/\/$/)) {
 // topの画像が読み込まれてから、スクロール可に(ログインボタンも消す)================================================
   $.when(
-    $('.top__nameBox').fadeIn(4000)
+    // $('.top__nameBox').fadeIn(4000)
   ).done(function(){
     $('.main').show();
     $('#footer').show();
@@ -10,31 +10,54 @@ $(function(){
   });
 
 // contents================================================
-  $('.works__contentsBox__photoBlock').hover(
-    function(){
-      $(this).children('.works__contentsBox__photoBlock__label').animate({bottom:'0'},200)
-      $(this).parents().next('.works__linkBox')
-        .css('backgroundColor','black')
-        .css('color','white')
-    },
-    function(){
-      $(this).children('.works__contentsBox__photoBlock__label').animate({bottom:'-20vh'},200)
-      $(this).parents().next('.works__linkBox')
-      .css('backgroundColor','white')
-      .css('color','black')
-    });
-// links================================================
-  $('.works__linkBox').hover(
-    function(){
-      $(this)
-        .css('backgroundColor','black')
-        .css('color','white')
-    },
-    function(){
-      $(this)
-        .css('backgroundColor','white')
-        .css('color','black')
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   $('.works__contentsBox__photoBlock').hover(
+//     function(){
+//       $(this).children('.works__contentsBox__photoBlock__label').animate({bottom:'0'},200)
+//       $(this).parents().next('.works__linkBox')
+//         .css('backgroundColor','black')
+//         .css('color','white')
+//     },
+//     function(){
+//       $(this).children('.works__contentsBox__photoBlock__label').animate({bottom:'-20vh'},200)
+//       $(this).parents().next('.works__linkBox')
+//       .css('backgroundColor','white')
+//       .css('color','black')
+//     });
+// // links================================================
+//   $('.works__linkBox').hover(
+//     function(){
+//       $(this)
+//         .css('backgroundColor','black')
+//         .css('color','white')
+//     },
+//     function(){
+//       $(this)
+//         .css('backgroundColor','white')
+//         .css('color','black')
+//   });
 // ================================================
   }
 });
