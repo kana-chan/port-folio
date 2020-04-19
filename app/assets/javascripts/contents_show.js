dc1a6ebc11df7a348a3ca6bbd5dcf86505d646c1
kana-chan <bkm0115_y@icloud.com>
2020-04-19 22:37:22 +0900

    show　page

--- app/assets/javascripts/contents_show.js
+++ app/assets/javascripts/contents_show.js
@@ -1,20 +1,21 @@
 $(function(){
   if (window.location.href.match(/\/contents\/\d+$/)) {
 // 初期値------------------------------------------------
-    $('.smallImage').children('.image0').siblings('.viewing').show();
-    $('.smallImage').children('.image0').css('filter','blur(2px)');
+    $('.smallImage').children('.index0').siblings('.viewing').show();
+    $('.smallImage').children('.index0').css('filter','blur(2px)');
+    $('.infomation').children('.index0').show();
 // app名、画像が読み込まれてからスクロール可================================================
-  // $('.upperBlock__name').fadeIn(3000)
-  // setTimeout(function(){
-  //   $.when(
-  //     $('.upperBlock__name').fadeOut(2000),
-  //     $('.upperBlock__overview').fadeIn(3000),
-  //     $('.upperBlock__contentBox').fadeIn(3000)
-  //   ).done(function(){
-  //     $('.lowerBlock').show();
-  //     $('#footer').show();
-  //   });
-  // },3000);
+  $('.upperBlock__name').fadeIn(3000)
+  setTimeout(function(){
+    $.when(
+      $('.upperBlock__name').fadeOut(2000),
+      $('.upperBlock__overview').fadeIn(3000),
+      $('.upperBlock__contentBox').fadeIn(3000)
+    ).done(function(){
+      $('.lowerBlock').show();
+      $('#footer').show();
+    });
+  },3000);
 // image hover================================================
   $('.smallImage').hover(
     function(){
@@ -29,12 +30,15 @@ $(function(){
       $('.smallImage').children('img').css('filter','blur(0)')
       $(this).children('.viewing').show();
       $(this).children('img').css('filter','blur(2px)')
+      $('.infomation').children('table').hide();
 // click でbigDisplayにうつされる------------------------------------------------
       var imageName = $(this).children('img').attr('class')
       var tergetPosition = $('.bigImage').children(`.${imageName}`).offset().left;
-      var basePosition = $('.bigImage').children('.image0').offset().left;
+      var basePosition = $('.bigImage').children('.index0').offset().left;
       var imagePosition = basePosition - tergetPosition
       $('.bigImage').animate({left:imagePosition})
+// click でinfomationが切り替わる------------------------------------------------
+      $('.infomation').children(`.${imageName}`).fadeIn();
     })
 // show moreボタン================================================
   $('.linkBtn').hover(
