3a1aac3ff8d512bd9c58446e7c7c36edcc06986a
kana-chan <bkm0115_y@icloud.com>
2020-04-19 18:00:47 +0900

    filter viewing

--- app/assets/javascripts/contents_show.js
+++ app/assets/javascripts/contents_show.js
@@ -1,28 +1,44 @@
 $(function(){
   if (window.location.href.match(/\/contents\/\d+$/)) {
-// app名、画像が読み込まれてからスクロール可------------------------------------------------
-  $('.upperBlock__name').fadeIn(3000)
-  setTimeout(function(){
-    $.when(
-      $('.upperBlock__name').fadeOut(2000),
-      $('.upperBlock__overview').fadeIn(3000),
-      $('.upperBlock__contentBox').fadeIn(3000)
-    ).done(function(){
-      $('.lowerBlock').show();
-      $('#footer').show();
-    });
-  },3000);
-
-// show moreボタン------------------------------------------------
-$('.linkBtn').hover(
-  function(){
-      $(this).children().children()
-        .animate({right:'-100%'},300)
-        .animate({right:'100%'},0)
-        .animate({right:'30%'},300)
+    $('.smallImage').children('.image0').siblings('.viewing').show();
+    $('.smallImage').children('.image0').css('filter','blur(2px)')
+// app名、画像が読み込まれてからスクロール可================================================
+  // $('.upperBlock__name').fadeIn(3000)
+  // setTimeout(function(){
+  //   $.when(
+  //     $('.upperBlock__name').fadeOut(2000),
+  //     $('.upperBlock__overview').fadeIn(3000),
+  //     $('.upperBlock__contentBox').fadeIn(3000)
+  //   ).done(function(){
+  //     $('.lowerBlock').show();
+  //     $('#footer').show();
+  //   });
+  // },3000);
+// image関係================================================
+// hover------------------------------------------------
+  $('.smallImage').hover(
+    function(){
+      $(this).children('img').animate({height:'13.5vw'})
     },
-    function(){});
-
+    function(){
+      $(this).children('img').animate({height:'13vw'})
+    });
+    // click------------------------------------------------
+    $('.smallImage').on('click',function(){
+      $('.viewing').hide()
+      $('.smallImage').children('img').css('filter','blur(0)')
+      $(this).children('.viewing').show();
+      $(this).children('img').css('filter','blur(2px)')
+    })
+// show moreボタン================================================
+  $('.linkBtn').hover(
+    function(){
+        $(this).children().children()
+          .animate({right:'-100%'},300)
+          .animate({right:'100%'},0)
+          .animate({right:'30%'},300)
+      },
+      function(){});
 // ------------------------------------------------
   }
 });
