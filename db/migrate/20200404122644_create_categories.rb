8de7f61c26564e89860ccf9fb1f8662c555cf23f
kana-chan <bkm0115_y@icloud.com>
2020-05-01 17:03:47 +0900

    update

--- db/migrate/20200404122644_create_categories.rb
+++ db/migrate/20200404122644_create_categories.rb
@@ -1,8 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[5.0]
   def change
     create_table :categories do |t|
-      t.string :name,                  null: false
-      t.string :text,                 null: false
+      t.string :name,                   null: false
+      t.string :text,                   null: false
       t.integer :number,                null: false
       t.string :icon
       t.timestamps
