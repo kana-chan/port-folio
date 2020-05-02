24d6f900706c81aaf0c31baa0633b91fd9806dd5
kana-chan <bkm0115_y@icloud.com>
2020-05-02 10:41:52 +0900

    rotate bars

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,5 +1,41 @@
 $(function(){
-// footerをおすと特定位置までジャンプ================================================
+// menubtnをおすと変形================================================
+  $('.menuBtn').on('click',function(){
+    $(this).toggleClass('active')
+    if ($(this).hasClass('active')){
+      $('.menuBtn--topBar')
+        .css('top','50%')
+        .css('transform','rotate(225deg)')
+      $('.menuBtn--underBar')
+        .css('top','50%')
+        .css('transform','rotate(-225deg)')
+      $('.menuBtn--centerBar').css('transform','rotate(135deg)')
+    }else{
+      $('.menuBtn--topBar')
+        .css('top','30%')
+        .css('transform','rotate(0)')
+      $('.menuBtn--underBar')
+        .css('top','70%')
+        .css('transform','rotate(0)')
+      $('.menuBtn--centerBar').css('transform','rotate(0)')
+    }
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // skillへ------------------------------------------------
 $('.footerLinks--skills').on('click',function(){
   var skills = $('.skills').offset().top;
