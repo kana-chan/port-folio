2ca03f8b292ddda946d309ec7b7f13104a19afc5
kana-chan <bkm0115_y@icloud.com>
2020-05-10 00:14:25 +0900

    change ditaie

--- app/assets/javascripts/contents_show.js
+++ app/assets/javascripts/contents_show.js
@@ -8,30 +8,30 @@ $(function(){
           .removeClass('active')
           .animate({width:'25vh',height:'25vh'})
           .css('backgroundColor','white')
-        $('.ditail__main__lowerBlock__contentBox').children('h3').show()
+        $('.ditail__main__lowerBlock__contentBox').children('h4').show()
         $('.ditail__main__lowerBlock__contentBox').children('.ditail__main__lowerBlock__contentBox--image')
           .animate({width:'23vh',height:'23vh'})
         $('.ditail__main__lowerBlock__contentBox').children('.ditail__main__lowerBlock__contentBox__textBox').animate({right:'50%'})
-        $('.ditail__main__lowerBlock__contentBox').children().children().children('h3,p').hide();
+        $('.ditail__main__lowerBlock__contentBox').children().children().children('h2,p').hide();
       }else{
         terget
           .addClass('active')
           .animate({width:'85vh',height:'35vh'})
           .css('backgroundColor','rgb(224, 68, 60)')
-        terget.children('h3').hide()
+        terget.children('h4').hide()
         terget.children('.ditail__main__lowerBlock__contentBox--image')
           .animate({width:'55vh',height:'33vh'})
         terget.children('.ditail__main__lowerBlock__contentBox__textBox').animate({right:'3%'})
-        terget.children().children().children('h3,p').fadeIn(1300);
+        terget.children().children().children('h2,p').fadeIn(1300);
 
         notTerget
           .removeClass('active')
           .animate({width:'15vh',height:'15vh'})
           .css('backgroundColor','white')
-        notTerget.children('h3').show()
+        notTerget.children('h4').show()
         notTerget.children('.ditail__main__lowerBlock__contentBox--image').animate({width:'13vh',height:'13vh'})
         notTerget.children('.ditail__main__lowerBlock__contentBox__textBox').animate({right:'50%'})
-        notTerget.children().children().children('h3,p').hide();
+        notTerget.children().children().children('h2,p').hide();
       }
     })
 // ------------------------------------------------
